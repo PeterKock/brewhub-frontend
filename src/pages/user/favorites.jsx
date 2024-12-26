34ef3b7b8c2aa29d7d8bbf32d4902dc8f2b83f28
Peter Kock <ppkock@gmail.com>
2024-12-26 13:06:20 +0100

    add favorites recipes test

--- src/pages/user/favorites.jsx
+++ src/pages/user/favorites.jsx
@@ -1,7 +1,7 @@
 export default function UserFavorites() {
     return (
         <div className="page User-favorites">
-            <h2>My Favorite Recipes</h2>
+            <h2>My Favorite Recipes TEST</h2>
         </div>
     );
 }
