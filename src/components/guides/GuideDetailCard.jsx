513ff1ba675b46827fb156662b878cf65b150764
Peter Kock <ppkock@gmail.com>
2025-01-23 17:54:27 +0100

    refactored Styles for the guides and inventory components

--- src/components/guides/GuideDetailCard.jsx
+++ src/components/guides/GuideDetailCard.jsx
@@ -1,5 +1,6 @@
 import PropTypes from 'prop-types';
 import { DetailHeader } from '../shared/DetailHeader';
+import './styles/GuideDetailCard.css'
 
 export const GuideDetailCard = ({ guide, onClose }) => {
 
